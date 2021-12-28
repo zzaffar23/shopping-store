e4011d05ff318d60f23845d19d8b7ab6705ecb80
Zee Zaffar <zzaffar2023@gmail.com>
2021-12-28 14:09:50 -0600

    Initial Commit

---
+++ DanFay.Store.Ids/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DanFay.Store.Ids.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
